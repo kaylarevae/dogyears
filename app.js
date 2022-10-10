7d6e5607d64c36bf9342363eb6c52dd9f995034a
kaylarevae <kaylarevae@gmail.com>
2022-10-10 15:19:26 -0500

    Complete Dog Years assignment from Codecademy

---
+++ app.js
@@ -0,0 +1,15 @@
+const myAge = 88;
+// I made a constant variable of a number 
+let earlyYears = 2;
+// This variable will change.
+earlyYears = earlyYears * 10.5;
+laterYears = myAge - 2;
+// The early years are accounted for with the earlyYears variable.
+laterYears *= 4;
+// The multiplication assignment operator can multiply and assign in one step.
+myAgeInDogYears = earlyYears + laterYears;
+// Added early years and later years for the human equivalent.
+let myName = 'Kayla Pins'.toLowerCase();
+// This will convert my name to all lower case letters.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years.`);
+// This will print the result to the console.
